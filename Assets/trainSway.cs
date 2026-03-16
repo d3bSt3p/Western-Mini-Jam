a858a2c2bbcd72085cc9307041e89c67a608d34d
d3bSt3p <strongcarpenter09@gmail.com>
2026-03-15 23:03:55 -0700

    tren

---
+++ Assets/trainSway.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SwayX : MonoBehaviour
+{
+    [SerializeField] private Animator charAnimator;
+    [SerializeField] private GameController gameController;
+    public float swayAmount = 0.5f;   
+    public float swaySpeed = 1f;      
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        charAnimator.SetFloat("Speed", Mathf.Abs(gameController.gameSpeed * 0.5f));
+        if (gameController.gameStarted)
+        {
+            float xOffset = Mathf.Sin(Time.time * swaySpeed) * swayAmount;
+            transform.position = startPosition + new Vector3(xOffset, 0f, 0f);
+        }
+    }
+}
